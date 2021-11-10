fd28b495af248f7346210e107eb48c805733cf65
unknown <ibmdev20@iiht.tech>
2021-11-10 21:17:34 +0800

    First Commit

---
+++ src/app/_models/timeSlots.ts
@@ -0,0 +1,7 @@
+import { Time } from "@angular/common";
+
+export class timeSlots{
+    id: string;
+    date: Date;
+    time: Time;
+}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,62 @@
+﻿import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { User } from '@app/_models';
+import { AccountService, AlertService } from '@app/_services';
+import { first } from 'rxjs/operators';
+
+@Component({ templateUrl: 'home.component.html' })
+export class HomeComponent {
+    user: User;
+    form: FormGroup;
+    loading = false;
+    submitted = false;
+
+    constructor(
+        private formBuilder: FormBuilder,
+        private accountService: AccountService,
+        private route: ActivatedRoute,
+        private router: Router,
+        private alertService: AlertService) 
+        {
+        this.user = this.accountService.userValue;
+    }
+
+    ngOnInit() {
+        this.form = this.formBuilder.group({
+            id: ['', Validators.required],
+            lastName: ['', Validators.required],
+        });
+    }
+
+    // convenience getter for easy access to form fields
+    get f() { return this.form.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+
+        // reset alerts on submit
+        this.alertService.clear();
+
+        // stop here if form is invalid
+        if (this.form.invalid) {
+            return;
+        }
+
+        this.loading = true;
+        this.accountService.registerTimeSlot(this.form.value)
+            .pipe(first())
+            .subscribe({
+                next: () => {
+                    this.alertService.success('Registration successful', { keepAfterRouteChange: true });
+                    const returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+                    this.router.navigateByUrl(returnUrl);
+                },
+                error: error => {
+                    this.alertService.error(error);
+                    this.loading = false;
+                }
+            });
+    }
+}
